24637cd8ee3a3d2eb7a68dee1a96f7ca911effd1
Yakov Borevich <jjay@ostrovok.ru>
2012-08-07 19:55:48 +0400

    Unicode suppport in ruby-scss <=> python

--- mediagenerator/filters/scss2.py
+++ mediagenerator/filters/scss2.py
@@ -5,19 +5,20 @@ process scss using ruby-scss.
 from subprocess import Popen, PIPE
 from StringIO import StringIO
 
+from django.utils.encoding import smart_str
+
 
 from mediagenerator.utils import find_file
 from mediagenerator.generators.bundles.base import FileFilter
 class ScssFilter(FileFilter):
     
     def get_dev_output(self, name, variation, content=None):
-
         if not content:
             content = super(ScssFilter, self).get_dev_output(name, variation)
 
-        command = ['/home/jjay/proj/sass/bin/scss', '--stdin', '--style=expanded', '-q']
+        command = ['scss', '--stdin', '--style=expanded', '-q']
         pipe = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = pipe.communicate(content)
+        stdout, stderr = pipe.communicate(smart_str(content))
         if stderr:
             raise RuntimeError("scss (%s) exited with error: %s" % (name, stderr))
 
