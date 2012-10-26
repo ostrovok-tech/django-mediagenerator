1c86b5333a52ee739729f99f9f414b0b1f2ff803
Yakov Borevich <jjay@ostrovok.ru>
2012-10-26 16:44:16 +0400

    Support optional scss path

--- mediagenerator/filters/scss2.py
+++ mediagenerator/filters/scss2.py
@@ -6,7 +6,7 @@ from subprocess import Popen, PIPE
 from StringIO import StringIO
 
 from django.utils.encoding import smart_str
-
+from django.conf import settings
 
 from mediagenerator.utils import find_file
 from mediagenerator.generators.bundles.base import FileFilter
@@ -15,8 +15,10 @@ class ScssFilter(FileFilter):
     def get_dev_output(self, name, variation, content=None):
         if not content:
             content = super(ScssFilter, self).get_dev_output(name, variation)
+
+        scss = getattr(settings, 'MEDIA_SCSS_PATH', 'scss')
         
-        command = ['scss', '--stdin', '--style=expanded', '-q']
+        command = [scss, '--stdin', '--style=expanded', '-q']
         pipe = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         stdout, stderr = pipe.communicate(smart_str(content))
         if stderr:
